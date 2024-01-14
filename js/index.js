ee8a7764041288f36e047f97b5ef601a12fb3721
TuNombre <patri.pascualr@alumnos.cei.es>
2024-01-14 23:58:27 +0100

    Mejoras responsive + Nuevas secciones de la web
    He creado nuevas secciones como un carrousel, el footer, nuevas secciones en la página Servicios, mejoras de responsive, etc

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
 
 
-/* Barra de navegación (se repite en todas las páginas)*/
+// Barra de navegación (Fetch para que se repita en todas las páginas de la web)
 
-// Barra de navegación (se repite en todas las páginas)
 fetch('/components/navbar.html')
     .then(response => response.text())
     .then(data => {
         document.querySelector('header').innerHTML = data;
 
         // Luego de cargar la barra de navegación, ejecuta el siguiente bloque de código
-        initNavbar();
+        initNavbar(); //Llamamos a la función para que se ejecute 
     })
-    .catch(error => console.error('Error fetching navbar:', error));
+    .catch
+
 
 // Función para inicializar el menú desplegable
 function initNavbar() {
@@ -23,6 +23,19 @@ function initNavbar() {
         navbar.classList.toggle('active');
     };
 }
+
+// Footer - Lo repetimos en todas las páginas de nuestra web
+
+fetch('/components/footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.querySelector('footer').innerHTML = data;
+    })
+    .catch
+
+
+
+
 
 
 
@@ -62,11 +75,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-
-
-
-
-
 // SECCIÓN "QUE HACEMOS" DE LA PÁG SERVICIOS (Desplegable) 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -82,3 +90,49 @@ document.addEventListener('DOMContentLoaded', function () {
       });
     });
 });
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+// Carrousel imgs
+
+//VARIABLES 
+const grande = document.querySelector('.carrousel-services_grande')
+const punto = document.querySelectorAll('.punto')
+
+ // Cuando hago click en cada punto
+    // Saber la posición de ese punto
+    // Aplicar un transform translateX al grande
+    // QUITAR la clase activo de TODOS los puntos
+    // AÑADIR la clase activo al punto que hemos hecho CLICK
+
+
+    punto.forEach( ( cadaPunto , i )=> { //Recorrer TODOS los puntos
+
+        punto[i].addEventListener('click',()=>{  // Asignamos un click a cadaPunto
+        
+            let posicion  = i //Guardamos la posición de ese punto
+            let operacion = posicion * -50 //Calculando el espacio que debe desplazarse el grande
+
+            //Movemos el grande = Le voy a decir: Cógeme el div "grande" dentro de su style, mete la propiedad de "transform" y le vas a aplicar un translate en el eje X(-50%), pero en vez de poner "-50%"" le diremos que añada la operación que será igual a ese valor" - Así conseguiremos que al hacer click en el botón se desplace la img del carrousel.
+            grande.style.transform = `translateX(${ operacion }%)`
+
+            //Para que el punto se marque tenemos que quitar la clase "activo" a todos los puntos y se la ponemos al que esté activo.
+            // Recorremos todos los puntos 
+            punto.forEach( ( cadaPunto , i )=>{
+                punto[i].classList.remove('activo') // Quitamos la clase ACTIVO a todos los puntos
+            })
+            punto[i].classList.add('activo') // Añadimos la clase ACTIVO en el punto al que hemos hecho click
+        })
+    })
